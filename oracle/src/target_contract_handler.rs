99e01e43bb605e257d23d48b7d10103f0b331b22
Jack E Bois <jack@flux.xyz>
2021-09-13 09:21:44 +0100

    fixed tests, removed all references to requester interface, replaced requestor with requester everywhere, fixed tests

--- oracle/src/target_contract_handler.rs
+++ oracle/src/target_contract_handler.rs
@@ -7,7 +7,7 @@
 
 #[ext_contract]
 pub trait TargetContractExtern {
-    fn set_outcome(request_id: U64, requestor: AccountId, outcome: Outcome, tags: Option<Vec<String>>, final_arbitrator_triggered: bool);
+    fn set_outcome(request_id: U64, requester: AccountId, outcome: Outcome, tags: Option<Vec<String>>, final_arbitrator_triggered: bool);
 }
 
 // #[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize)]
@@ -19,14 +19,14 @@ const GAS_BASE_SET_OUTCOME: Gas = 250_000_000_000_000;
     pub fn set_outcome(
         &self,
         request_id: U64,
-        requestor: AccountId,
+        requester: AccountId,
         outcome: Outcome,
         tags: Option<Vec<String>>,
         final_arbitrator_triggered: bool
     ) -> Promise {
         target_contract_extern::set_outcome(
             request_id,
-            requestor,
+            requester,
             outcome,
             tags,
             final_arbitrator_triggered,
