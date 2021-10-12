f0a2f4883563995e15d1d6f1eb19ddc11f707cb2
Jameson Hodge <contact@jamesonhodge.com>
2021-10-12 09:37:48 -0400

    use FungibleTokenReciever trait from near_contract_standards

--- oracle/src/fungible_token_receiver.rs
+++ oracle/src/fungible_token_receiver.rs
@@ -4,18 +4,17 @@ use near_sdk::{
     serde_json,
     env,
     PromiseOrValue,
-    // AccountId,
+    json_types::ValidAccountId,
     near_bindgen,
-    // json_types::{ U128 }
 };
+use near_contract_standards::
+    fungible_token::
+    receiver::
+    FungibleTokenReceiver;
 use flux_sdk::{
     data_request::{ NewDataRequestArgs, StakeDataRequestArgs },
     types::WrappedBalance
 };
-// use near_contract_standards::
-//     fungible_token::
-//     receiver::
-//     FungibleTokenReceiver;
 
 
 #[derive(Serialize, Deserialize)]
@@ -24,35 +23,28 @@ pub enum Payload {
     StakeDataRequest(StakeDataRequestArgs)
 }
 
-// AUDIT: This trait is also available from https://github.com/near/near-sdk-rs/tree/master/near-contract-standards
-// SOLUTION: replace with near-built functionality, BUT GETTING IRRECONCILABLE TYPE ERRORS, need to replace token
-// implementation with near fungible token implementation
-pub trait FungibleTokenReceiver {
-    // @returns amount of unused tokens
-    fn ft_on_transfer(&mut self, sender_id: AccountId, amount: U128, msg: String) -> PromiseOrValue<WrappedBalance>;
-}
-
 #[near_bindgen]
 impl FungibleTokenReceiver for Contract {
     // @returns amount of unused tokens
     fn ft_on_transfer(
         &mut self,
-        sender_id: near_sdk::AccountId,
-        amount: near_sdk::json_types::U128,
+        sender_id: ValidAccountId,
+        amount: U128,
         msg: String
     ) -> PromiseOrValue<WrappedBalance> {
+        let sender: &AccountId = sender_id.as_ref();
         // AUDIT: Verify that the call is made from the correct fungible token by comparing predecessor_account_id
         // SOLUTION: implement predecessor_account_id check
         let initial_storage_usage = env::storage_usage();
-        let account = self.get_storage_account(&sender_id);
+        let account = self.get_storage_account(&sender);
 
         let payload: Payload =  serde_json::from_str(&msg).expect("Failed to parse the payload, invalid `msg` format");
         let unspent = match payload {
-            Payload::NewDataRequest(payload) => self.ft_dr_new_callback(sender_id.clone(), amount.into(), payload).into(),
-            Payload::StakeDataRequest(payload) => self.dr_stake(sender_id.clone(), amount.into(), payload),
+            Payload::NewDataRequest(payload) => self.ft_dr_new_callback(sender.clone(), amount.into(), payload).into(),
+            Payload::StakeDataRequest(payload) => self.dr_stake(sender.clone(), amount.into(), payload),
         };
 
-        self.use_storage(&sender_id, initial_storage_usage, account.available);
+        self.use_storage(&sender, initial_storage_usage, account.available);
 
         unspent
     }
@@ -171,7 +163,7 @@ mod mock_token_basic_tests {
                 "outcome": Outcome::Answer(AnswerType::String("a".to_string()))
             }
         });
-        contract.ft_on_transfer(alice(), U128(100), msg.to_string());
+        contract.ft_on_transfer(alice().try_into().unwrap(), U128(100), msg.to_string());
     }
 
     #[test]
@@ -203,7 +195,7 @@ mod mock_token_basic_tests {
                 "outcome": Outcome::Answer(AnswerType::String("a".to_string()))
             }
         });
-        contract.ft_on_transfer(alice(), U128(100), msg.to_string());
+        contract.ft_on_transfer(alice().try_into().unwrap(), U128(100), msg.to_string());
 
         let account = contract.accounts.get(&alice());
         assert!(account.unwrap().available < storage_start);
